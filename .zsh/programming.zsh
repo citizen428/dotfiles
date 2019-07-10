95e879c2bba48442aeb454fa37358a9de806cef0
Michael Kohl <citizen428@gmail.com>
2019-07-10 23:13:30 +0700

    Add dotnet core tools to PATH

--- .zsh/programming.zsh
+++ .zsh/programming.zsh
@@ -12,3 +12,6 @@ path=($GOPATH/bin $path)
 # opam configuration
 test -r ~/.opam/opam-init/init.zsh && . ~/.opam/opam-init/init.zsh > /dev/null 2> /dev/null || true
 
+# .NET Core
+path=($HOME/.dotnet/tools $path)
+
