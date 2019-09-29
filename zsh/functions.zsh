d82c07b4092e519803e4dfe5cb0c985a3d5e3e87
Michael Kohl <citizen428@gmail.com>
2019-09-29 21:31:21 +0700

    Add clone() function to shell config

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -7,3 +7,8 @@ function brew-deps() {
     echo -n "\e[1;34m$cask ->\e[0m"; brew deps $cask | awk '{printf(" %s ", $0)}'; echo "";
   done
 }
+
+function clone() {
+  git clone $1
+  cd $(basename ${1%.*})
+}
