13b9f824536778e67c757669e99bda5f5b35481f
Michael Kohl <me@citizen428.net>
2022-07-06 15:21:18 +0700

    Update install script

--- install.sh
+++ install.sh
@@ -4,9 +4,14 @@
 # bash 3 by default, so ksh it is.
 
 if [[ ! $(command -v stow &> /dev/null) ]]; then
-  echo "stow not available, aborting."
-  exit 1
+	echo "stow not available, aborting."
+	exit 1
 fi
+
+function stow_config {
+	echo "Linking ${config} in ${directory}"
+	stow -t $1 $2
+}
 
 typeset -A configs=(
 	[asdf]=~ 
@@ -24,14 +29,26 @@ typeset -A configs=(
 	[zsh]=~
 )
 
-for config in "${!configs[@]}"; do
-  directory=${configs[$config]}
-  read -n1 confirm?"Install ${config}? [y/n] " 
-  echo
-  if [[ "$confirm" = "y" || "$confirm" = "Y" ]]; then
-    echo "Linking ${config} in ${directory}"
-    stow -t $directory $config
-  else
-    echo "Skipping ${config}"
-  fi
-done
+if [[ $# -gt 0 ]]; then
+	# We got a list of configs on the command line
+	for config in $@; do
+	  directory=${configs[$config]}
+	  if [[ -n "${directory}" ]] ; then
+	    stow_config $directory $config
+	  else
+	    echo "Unknown config ${config}, check spelling"
+	  fi
+	done
+else
+	# Interactively confirm each known config
+	for config in "${!configs[@]}"; do
+	  directory=${configs[$config]}
+	  read -n1 confirm?"Install ${config}? [y/n] " 
+	  echo
+	  if [[ "$confirm" = "y" || "$confirm" = "Y" ]]; then
+	    stow_config $directory $config
+	  else
+	    echo "Skipping ${config}"
+	  fi
+	done
+fi
