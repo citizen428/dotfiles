0a48f60bf8871a63575f843bc8c78d811a3057f5
Michael Kohl <me@citizen428.net>
2022-08-12 14:27:15 +0700

    Remove iTerm shell integration

--- fish/fish/config.fish
+++ fish/fish/config.fish
@@ -3,5 +3,3 @@ set -x EDITOR vim
 ### UI
 set fish_pager_color_progress --background='red'
 starship init fish | source
-
-test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish ; or true
