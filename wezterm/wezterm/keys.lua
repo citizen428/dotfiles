7e70535e4ada713b7b51980969e25281f2d39dc9
Michael Kohl <me@citizen428.net>
2024-08-19 16:25:36 +0700

    Refactor key mappings

--- wezterm/wezterm/keys.lua
+++ wezterm/wezterm/keys.lua
@@ -1,54 +1,56 @@
-local act = require("wezterm").action
+local wezterm = require("wezterm")
+local act = wezterm.action
+
+local function map(mods, key, action)
+	return { mods = mods, key = key, action = action }
+end
+
+local function resize_pane(key, direction)
+	return { key = key, action = act.AdjustPaneSize({ direction, 5 }) }
+end
 
 return {
 	setup = function(config)
-		config.leader = { key = "b", mods = "CTRL" }
+		config.leader = { mods = "CTRL", key = "a" }
+
 		config.keys = {
+			-- Send CTRL-A in leader mode by quickly pressing it twice
+			map("LEADER|CTRL", "a", act.SendKey({ mods = "CTRL", key = "a" })),
 			-- Make Option-Left equivalent to Alt-b; backward-word
-			{ key = "LeftArrow", mods = "OPT", action = act.SendString("\x1bb") },
+			map("OPT", "LeftArrow", act.SendString("\x1bb")),
 			-- Make Option-Right equivalent to Alt-f; forward-word
-			{ key = "RightArrow", mods = "OPT", action = act.SendString("\x1bf") },
-			{
-				key = "w",
-				mods = "CMD",
-				action = act.CloseCurrentPane({ confirm = true }),
-			},
-			{
-				key = "|",
-				mods = "CTRL|SHIFT",
-				action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-			},
-			{
-				key = "}",
-				mods = "CTRL|SHIFT",
-				action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-			},
-			{ key = "=", mods = "CTRL", action = act.PaneSelect },
-			{
-				key = "-",
-				mods = "CTRL",
-				action = act.PaneSelect({ mode = "SwapWithActive" }),
-			},
-			{
-				key = "h",
-				mods = "LEADER",
-				action = act.AdjustPaneSize({ "Left", 5 }),
-			},
-			{
-				key = "j",
-				mods = "LEADER",
-				action = act.AdjustPaneSize({ "Down", 5 }),
-			},
-			{ key = "k", mods = "LEADER", action = act.AdjustPaneSize({ "Up", 5 }) },
-			{
-				key = "l",
-				mods = "LEADER",
-				action = act.AdjustPaneSize({ "Right", 5 }),
-			},
-			{
-				key = "l",
-				mods = "ALT",
-				action = act.ShowLauncherArgs({ flags = "FUZZY|TABS|DOMAINS" }),
+			map("OPT", "RightArrow", act.SendString("\x1bf")),
+			map("CMD", "w", act.CloseCurrentPane({ confirm = true })),
+			map("CTRL|SHIFT", "|", act.SplitHorizontal({ domain = "CurrentPaneDomain" })),
+			map("CTRL|SHIFT", "}", act.SplitVertical({ domain = "CurrentPaneDomain" })),
+			map("CTRL", "=", act.PaneSelect),
+			map("CTRL", "-", act.PaneSelect({ mode = "SwapWithActive" })),
+			map("ALT", "l", act.ShowLauncherArgs({ flags = "FUZZY|TABS|DOMAINS" })),
+			map("ALT", "w", act.ShowLauncherArgs({ flags = "WORKSPACES" })),
+			map(
+				"LEADER",
+				"r",
+				act.ActivateKeyTable({ name = "resize_panes", one_shot = false, timeout_milliseconds = 500 })
+			),
+			map(
+				"LEADER",
+				"t",
+				wezterm.action_callback(function(window, _)
+					if window:get_config_overrides()["color_scheme"] ~= nil then
+						window:set_config_overrides({})
+					else
+						window:set_config_overrides({ color_scheme = "iTerm2 Solarized Light" })
+					end
+				end)
+			),
+		}
+
+		config.key_tables = {
+			resize_panes = {
+				resize_pane("h", "Left"),
+				resize_pane("j", "Down"),
+				resize_pane("k", "Up"),
+				resize_pane("l", "Right"),
 			},
 		}
 	end,
