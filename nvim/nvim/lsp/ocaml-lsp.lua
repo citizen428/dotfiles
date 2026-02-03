3d4725281f6c98f797e43c03616f636f145bc127
Michael Kohl <me@citizen428.net>
2026-02-03 22:10:52 +0700

    nvim: Fix OCaml LSP config

--- nvim/nvim/lsp/ocaml-lsp.lua
+++ nvim/nvim/lsp/ocaml-lsp.lua
@@ -1,5 +1,5 @@
-vim.lsp.config("ocaml-lsp", {
+return {
   cmd = { "ocamllsp" },
   filetypes = { "ocaml", "ocaml.menhir", "ocaml.interface", "ocamllex", "reason", "dune" },
   root_dir = vim.fs.root(0, { "dune-project", "dune-workspace", "*.opam" }),
-})
+}
