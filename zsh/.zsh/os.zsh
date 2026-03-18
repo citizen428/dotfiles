f622892adb792944d24aff8497440e4d36e3359e
Michael Kohl <me@citizen428.net>
2026-03-18 16:19:26 +0700

    zsh: Add Linux to os.zsh

--- zsh/.zsh/os.zsh
+++ zsh/.zsh/os.zsh
@@ -3,21 +3,25 @@ if [[ $OS = 'Linux' && -f /etc/wsl.conf ]]; then
     export OS="WSL"
 fi
 
-if [[ $OS = "Darwin" ]]; then
-    export HOMEBREW_PREFIX=/opt/homebrew
-    export HOMEBREW_NO_ENV_HINTS=true
-    export CPPFLAGS="$CPPFLAGS -I $HOMEBREW_PREFIX/include"
-    export LDFLAGS="$LDFLAGS -L $HOMEBREW_PREFIX/lib"
-    znap eval homebrew "${HOMEBREW_PREFIX}/bin/brew shellenv"
-fi
+case $OS in
+    Darwin)
+        export HOMEBREW_PREFIX=/opt/homebrew
+        export HOMEBREW_NO_ENV_HINTS=true
+        export CPPFLAGS="$CPPFLAGS -I $HOMEBREW_PREFIX/include"
+        export LDFLAGS="$LDFLAGS -L $HOMEBREW_PREFIX/lib"
+        znap eval homebrew "${HOMEBREW_PREFIX}/bin/brew shellenv"
+        ;;
+    Linux)
+        export HOMEBREW_NO_ENV_HINTS=true
+        ;;
+    WSL)
+        znap eval keychain "keychain --noask --quiet --eval --agents ssh"
 
-if [[ $OS = "WSL" ]]; then
-    znap eval keychain "keychain --noask --quiet --eval --agents ssh"
+        path=('/snap/bin' $path)
+        path=('/mnt/c/Users/me/AppData/Local/Programs/Microsoft VS Code/bin/' $path)
 
-    path=('/snap/bin' $path)
-    path=('/mnt/c/Users/me/AppData/Local/Programs/Microsoft VS Code/bin/' $path)
-
-    export HOST_IP=$(ip route |awk '/^default/{print $3}')
-    export PULSE_SERVER="tcp:$HOST_IP"
-    export DISPLAY="$HOST_IP:0.0"
-fi
+        export HOST_IP=$(ip route |awk '/^default/{print $3}')
+        export PULSE_SERVER="tcp:$HOST_IP"
+        export DISPLAY="$HOST_IP:0.0"
+        ;;
+esac
