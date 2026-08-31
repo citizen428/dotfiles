1de020328f2436de697e3b069ff23a10b9b380aa
Michael Kohl <me@citizen428.net>
2026-08-31 10:23:04 +0700

    emacs: Add missing configs and refactor

--- home/private_dot_emacs.d/init.el
+++ home/private_dot_emacs.d/init.el
@@ -14,85 +14,83 @@
   (setq auto-save-file-name-transforms `((".*" ,auto-save-dir t)))
   (setq create-lockfiles nil))
 
-(setq
- initial-scratch-message nil
- inhibit-startup-screen t
- server-client-instructions nil)
-
-;;; Editing / Behavior
-
-;; Show column in modeline
-(column-number-mode 1)
-
-(setq
- require-final-newline t
- ring-bell-function 'ignore)
-
-;; Automatically reload files changed outside Emacs
-(global-auto-revert-mode)
-
-;; Completion settings
-(setq
- tab-always-indent 'complete
- completion-auto-select 'second-tab
- completion-styles '(basic flex)
- completions-format 'one-column
- completions-max-height 20)
-(global-completion-preview-mode)
-(fido-vertical-mode)
-
-;; Whitespace handling
-(setq
- whitespace-line-column 100
- whitespace-style '(face lines-tail))
-(global-whitespace-mode)
-(add-hook 'before-save-hook #'delete-trailing-whitespace)
-
 ;;; Keys
 
 (when (eq system-type 'darwin)
   (setq mac-command-modifier 'meta)
   (setq mac-option-modifier 'super))
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-(global-set-key (kbd "C-x C-b") 'ibuffer)
+(global-set-key (kbd "<escape>") #'keyboard-escape-quit)
+(global-set-key (kbd "C-x C-b") #'ibuffer)
+(global-set-key (kbd "M-`") #'other-frame)
 
+;; My prefix keys (C-z)
 (define-prefix-command 'my-prefix-map)
+(global-set-key (kbd "C-<tab>") #'previous-buffer)
 (global-set-key (kbd "C-z") 'my-prefix-map)
 (define-key my-prefix-map (kbd "b") #'browse-url-at-point)
 (define-key my-prefix-map (kbd "m") #'mu4e)
-(define-key my-prefix-map (kbd "f f") #'project-find-file)
-(define-key my-prefix-map (kbd "f r") #'recentf-open)
-(define-key my-prefix-map (kbd "t n") #'display-line-numbers-mode)
-(define-key my-prefix-map (kbd "t h") #'hl-line-mode)
+(define-prefix-command 'my-prefix-file-map)
+(define-key my-prefix-map (kbd "f") 'my-prefix-file-map)
+(define-key my-prefix-file-map (kbd "f") #'project-find-file)
+(define-key my-prefix-file-map (kbd "r") #'recentf-open)
+(define-prefix-command 'my-prefix-toggle-map)
+(define-key my-prefix-map (kbd "t") 'my-prefix-toggle-map)
+(define-key my-prefix-toggle-map (kbd "n") #'display-line-numbers-mode)
+(define-key my-prefix-toggle-map (kbd "h") #'hl-line-mode)
+(define-key my-prefix-toggle-map (kbd "w") #'which-key-mode)
 
-;;; Packages
+;;; Internal packages (part of Emacs)
 
-(use-package diminish
-  :config
-  (diminish 'buffer-terminator-mode)
-  (diminish 'completion-preview-mode)
-  (diminish 'whitespace-mode))
+(use-package emacs
+  :ensure nil
+  :demand t
+  :custom
+  ;; Be less verbose
+  (initial-scratch-message nil)
+  (inhibit-startup-screen t)
+  (server-client-instructions nil)
+  ;; Modes
+  (context-menu-mode t)
+  (column-number-mode t)
+  (delete-selection-mode t) ; Typing replaces current selection
+  (editorconfig-mode t)
+  (electric-pair-mode t)
+  (global-xref-mouse-mode t)
+  (pixel-scroll-mode t) ; Smoother scrolling
+  (repeat-mode t) ; Skip prefix on repeat invocations for certain commands
+  (savehist-mode t)
+  ;; Mouse
+  (mouse-drag-and-drop-region t)
+  (mouse-drag-and-drop-region-cross-program t)
+  (mouse-yank-at-point t)
+  ;; Misc
+  (dired-auto-revert-buffer t)
+  (dired-mouse-drag-files t) ; C-left: copy, S-left: move, M-left: link
+  (global-auto-revert-mode t) ; Reload files when changed externally
+  (imenu-auto-rescan t) ; Update imenu based on current buffer
+  (require-final-newline t)
+  (ring-bell-function 'ignore)
+  (shell-command-prompt-show-cwd t)
+  (view-read-only t))
 
-(use-package auto-dark
+(use-package completion-preview
+  :ensure nil
+  :demand t
   :diminish
+  :bind
+  (:map completion-preview-active-mode-map
+	("M-i" . completion-preview-insert-word)
+	("M-n" . completion-preview-next-candidate)
+	("M-p" . completion-preview-prev-candidate)
+	("M-<return>" . completion-preview-insert)
+	("<tab>" . completion-preview-complete))
   :custom
-  (auto-dark-themes '((modus-vivendi-tinted) (modus-operandi-tinted)))
+  (completion-preview-minimum-symbol-length 2)
+  (completion-show-help nil)
+  (completion-styles '(basic flex))
   :config
-  (defun my/server-auto-dark (frame)
-    (with-selected-frame frame
-      (when (display-graphic-p)
-        (auto-dark-mode 1)
-        (remove-hook 'after-make-frame-functions #'my/server-auto-dark))))
-  (add-hook 'after-make-frame-functions #'my/server-auto-dark))
-
-(use-package buffer-terminator
-  :custom
-  (buffer-terminator-verbose nil)
-  (buffer-terminator-inactivity-timeout (* 2 60 60)) ; 2 hours
-  (buffer-terminator-interval (* 30 60)) ; 30 minutes
-  :init
-  (buffer-terminator-mode 1))
+  (global-completion-preview-mode 1))
 
 (use-package eglot
   :ensure nil
@@ -101,70 +99,36 @@
   (eglot-sync-connect nil)
   (eglot-events-buffer-config '(:size 0)))
 
-(use-package exec-path-from-shell
-  :config
-  (when (daemonp)
-    (dolist (var '("SSH_AUTH_SOCK"))
-      (add-to-list 'exec-path-from-shell-variables var))
-    (exec-path-from-shell-initialize)))
-
-(use-package ghostel
+(use-package minibuffer
+  :ensure nil
+  :demand t
   :bind
-  ("C-`" . my/toggle-ghostel-panel)
-  :init
-  (defun my/toggle-ghostel-panel ()
-    "Toggle a ghostel terminal in a side window along the bottom of the frame."
-    (interactive)
-    (if-let* ((win (seq-find
-                    (lambda (w)
-                      (and (eq (window-parameter w 'window-side) 'bottom)
-                           (with-current-buffer (window-buffer w)
-                             (derived-mode-p 'ghostel-mode))))
-                    (window-list))))
-        (delete-window win)
-      (let ((display-buffer-overriding-action
-             '((display-buffer-in-side-window)
-               (side . bottom)
-               (slot . 0)
-               (window-height . 0.33)
-               (dedicated . t)
-               (preserve-size . (nil . t)))))
-        (ghostel-project)))))
-
-(use-package magit
-  :bind
-  (:map
-   my-prefix-map
-   ("g" . magit-status)))
-
-(load-file (locate-user-emacs-file "mu4e.el"))
-
-(use-package obsidian
-  :init
-  (define-prefix-command 'my-obsidian-map)
-  (define-key my-prefix-map "o" 'my-obsidian-map)
-  :config
-  (global-obsidian-mode t)
-  (obsidian-backlinks-mode t)
+  (:map completion-in-region-mode-map
+	("M-i" . minibuffer-choose-completion)
+	("M-n" . minibuffer-next-completion)
+	("M-p" . minibuffer-previous-completion))
   :custom
-  (obsidian-directory "~/Obsidian")
-  ;; Default location for new notes from `obsidian-capture'
-  (obsidian-inbox-directory "Inbox")
-  :bind
-  (:map my-obsidian-map
-        ("n" . obsidian-capture)
-        ("l" . obsidian-insert-link)
-        ("o" . obsidian-follow-link-at-point)
-        ("s" . obsidian-jump)
-        ("b" . obsidian-backlink-jump)))
+  (completions-auto-help t)
+  (completions-detailed t)
+  (completion-eager-update t)
+  (completions-format 'one-column)
+  (completions-max-height 15)
+  (completions-sort 'historical)
+  (minibuffer-visible-completions t))
 
-;; TODO: Update to Non-GNU ELPA once it releases there.
-(use-package tramp-rpc
-  :after tramp
-  :vc (:url "https://github.com/ArthurHeymans/emacs-tramp-rpc"
-       :rev :newest
-       :lisp-dir "lisp"))
+(use-package whitespace
+  :ensure nil
+  :demand t
+  :custom
+  (whitespace-line-column 100)
+  (whitespace-style '(face lines-tail))
+  :config
+  (add-hook 'before-save-hook #'delete-trailing-whitespace)
+  (add-hook 'prog-mode #'whitespace-mode))
 
-(use-package zig-mode)
+;;; External packages
+
+(load-file (locate-user-emacs-file "packages.el"))
+(load-file (locate-user-emacs-file "mu4e.el"))
 
 (message "Emacs loaded in: %s" (emacs-init-time))
---
+++ home/private_dot_emacs.d/packages.el
@@ -0,0 +1,90 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package diminish
+  :config
+  (diminish #'whitespace-mode)
+  (diminish #'which-key-mode))
+
+(use-package auto-dark
+  :diminish
+  :custom
+  (auto-dark-themes '((modus-vivendi-tinted) (modus-operandi-tinted)))
+  :config
+  (defun my/server-auto-dark (frame)
+    (with-selected-frame frame
+      (when (display-graphic-p)
+        (auto-dark-mode 1)
+        (remove-hook 'after-make-frame-functions #'my/server-auto-dark))))
+  (add-hook 'after-make-frame-functions #'my/server-auto-dark))
+
+(use-package buffer-terminator
+  :diminish
+  :custom
+  (buffer-terminator-verbose nil)
+  (buffer-terminator-inactivity-timeout (* 2 60 60)) ; 2 hours
+  (buffer-terminator-interval (* 30 60)) ; 30 minutes
+  :init
+  (buffer-terminator-mode 1))
+
+(use-package exec-path-from-shell
+  :config
+  (when (daemonp)
+    (dolist (var '("SSH_AUTH_SOCK"))
+      (add-to-list 'exec-path-from-shell-variables var))
+    (exec-path-from-shell-initialize)))
+
+(use-package ghostel
+  :bind
+  ("C-`" . my/toggle-ghostel-panel)
+  :init
+  (defun my/toggle-ghostel-panel ()
+    "Toggle a ghostel terminal in a side window along the bottom of the frame."
+    (interactive)
+    (if-let* ((win (seq-find
+                    (lambda (w)
+                      (and (eq (window-parameter w 'window-side) 'bottom)
+                           (with-current-buffer (window-buffer w)
+                             (derived-mode-p 'ghostel-mode))))
+                    (window-list))))
+        (delete-window win)
+      (let ((display-buffer-overriding-action
+             '((display-buffer-in-side-window)
+               (side . bottom)
+               (slot . 0)
+               (window-height . 0.33)
+               (dedicated . t)
+               (preserve-size . (nil . t)))))
+        (ghostel-project)))))
+
+(use-package magit
+  :bind
+  (:map my-prefix-map
+	("g" . magit-status)))
+
+(use-package obsidian
+  :init
+  (define-prefix-command 'my-obsidian-map)
+  (define-key my-prefix-map "o" 'my-obsidian-map)
+  :config
+  (global-obsidian-mode t)
+  (obsidian-backlinks-mode t)
+  :custom
+  (obsidian-directory "~/Obsidian")
+  ;; Default location for new notes from `obsidian-capture'
+  (obsidian-inbox-directory "Inbox")
+  :bind
+  (:map my-obsidian-map
+        ("n" . obsidian-capture)
+        ("l" . obsidian-insert-link)
+        ("o" . obsidian-follow-link-at-point)
+        ("s" . obsidian-jump)
+        ("b" . obsidian-backlink-jump)))
+
+;; TODO: Update to Non-GNU ELPA once it releases there.
+(use-package tramp-rpc
+  :after tramp
+  :vc (:url "https://github.com/ArthurHeymans/emacs-tramp-rpc"
+       :rev :newest
+       :lisp-dir "lisp"))
+
+(use-package zig-mode)
